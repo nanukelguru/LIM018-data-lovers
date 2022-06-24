7c31bfba55b7935db5b12903ad4dcadae6d78892
fiorellitamaya <fiorellitamaya@hotmail.com>
2022-06-23 19:19:54 -0500

    Modificaciones en CSS

--- src/main.js
+++ src/main.js
@@ -2,15 +2,11 @@ import {showFilms, sortAz} from './data.js';
 import data from './data/ghibli/ghibli.js';
 // console.log(films, data);
 
-// let dataFilms;               //<----Jalando toda la Data desde archivo json
+// let dataFilms;             //<----Jalando toda la Data desde archivo json
 // fetch('./data/ghibli/ghibli.json')
 // .then(studio => studio.json())
 // .then(data=>{
 //   dataFilms = data.films;
-
-
-// Jalando todo los links de los posters de la data
-// const posters = dataFilms.map(dataFilms => (dataFilms.poster));
 
 showFilms(data.films)
 
@@ -19,15 +15,13 @@ AZ.addEventListener("click", function(){
    showFilms(sortAz(data.films))
 })
 
-
-
 let linkPeliculas = document.getElementById("linkPeliculas")
-let linkInicio = document.getElementById("linkInicio")
+let linkInicio = document.getElementById("linkInicio2")
 let home = document.getElementById("home")
 let films1 = document.getElementById("films")
 
-/* Mostrar films y remover encabezado1*/
-linkPeliculas.addEventListener('click',() => {
+
+linkPeliculas.addEventListener('click',() => { // <---- Mostrar films y remover encabezado1
     home.style.display = "none";
     films1.style.display = "inline";
 })
